af59132779b9349443ef8146f8c4f3c950be774d
Thiago Castilho <tcastilho@outlook.com>
2020-01-27 13:51:16 -0300

    files from section 07

---
+++ section07/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+    AuthModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AppModule {}
---
+++ section07/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
+  const app = await NestFactory.create(AppModule);
+  const port  = 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
