f285322f59b2037fd0f20e5753378dad3bca9928
Thiago Castilho <tcastilho@outlook.com>
2020-01-27 16:22:58 -0300

    files from section 10

---
+++ section10/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+import * as config from 'config';
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`)
+  }
+
+  const app = await NestFactory.create(AppModule);
+  const port  = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
